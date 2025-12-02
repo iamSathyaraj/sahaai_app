1154f909ad7d37e29c453bded879774abc76024e
iamSathyaraj <sathyarajk001@gmail.com>
2025-12-02 23:35:17 +0530

    changes in app setups
    - Added provider, dio, flutter_secure_storage dependencies, google_maps_flutter
    - main.dart DI setup
    - AppRouter with auth routes
    - pubspec.lock exact versions

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,8 +1,46 @@
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:sahaai/features/auth/domain/usecases/otp_verify_usecase.dart';
+import 'package:sahaai/features/auth/presentation/pages/forgot_password_screen.dart';
+import 'package:sahaai/features/auth/presentation/pages/login_page.dart';
+import 'package:sahaai/features/auth/presentation/pages/otp_verify_page.dart';
+import 'package:sahaai/features/auth/presentation/pages/register_page.dart';
+import 'package:sahaai/features/onboarding/presentation/pages/role_selection_screen.dart';
+import 'package:sahaai/features/splash/presentation/pages/splash_screen.dart';
 
 class AppRouter {
   static final router = GoRouter(
     initialLocation: '/splash', 
-    routes: [],
+    routes: [
+        GoRoute(
+        path: '/splash',
+        builder: (context, state) => const SplashScreen(),
+      ),
+        GoRoute(
+        path: '/login',
+        builder: (context, state) =>  LoginPage(),
+      ),
+        GoRoute(
+        path: '/roleSelection',
+        builder: (context, state) =>  RoleSelectionScreen(),
+      ),
+        GoRoute(
+        path: '/registration',
+        builder: (context, state) =>  RegisterPage(),
+      ),
+        GoRoute(
+        path: '/forgotpassword',
+        builder: (context, state) =>  ForgotPasswordScreen(),
+      ),
+      GoRoute(
+  path: '/otp-verify/:userId',
+  builder: (context, state) {
+    final userIds = state.pathParameters['userId']!;
+        final userId = int.tryParse(userIds);
+    return OtpVerificationPage(userId: userId!);
+  },
+),
+
+    ],
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
-import 'package:flutter/material.dart';
-import 'app.dart';
-void main() {
-  runApp(const App());
-}
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sahaai/app.dart';
+import 'package:sahaai/core/network/api_service.dart';
+import 'package:sahaai/core/network/dio_client.dart';
+import 'package:sahaai/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:sahaai/features/auth/domain/usecases/login_usecase.dart';
+import 'package:sahaai/features/auth/domain/usecases/otp_verify_usecase.dart';
+import 'package:sahaai/features/auth/domain/usecases/register_usecase.dart';
+import 'package:sahaai/features/auth/domain/usecases/register_worker_usecase.dart';
+import 'package:sahaai/features/auth/presentation/provider/login_provider.dart';
+import 'package:sahaai/features/auth/presentation/provider/otp_provider.dart';
+import 'package:sahaai/features/auth/presentation/provider/register_provider.dart';
+import 'package:sahaai/features/home/data/repositories/home_repository_impl.dart';
+import 'package:sahaai/features/home/domain/usecases/get_service_usecase.dart';
+
+void main() {
+  final client = DioClient(baseUrl: "http://192.168.1.90:5016/api/");
+  final authService = AuthService(client.dio);
+  final authRepository = AuthRepositoryImpl(authService);
+  final homeService = HomeService(client.dio);
+  final homeRepository = HomeRepositoryImpl(homeService);
+  final getServicesUseCase = GetServiceUsecase(homeRepository);
+  final registerUseCase = RegisterUseCase(authRepository);
+  final registerWorkerUseCase = RegisterWorkerUseCase(authRepository);
+  final loginUseCase = LoginUseCase(authRepository);
+  final otpVerifyUseCase = VerifyOtpUseCase(authRepository);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => RegisterProvider(registerUseCase, registerWorkerUseCase),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(loginUseCase),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => OtpProvider(otpVerifyUseCase),
+        ),
+      ],
+      child: const App(),
+    ),
+  );
+}
 
